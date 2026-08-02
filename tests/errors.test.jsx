7fc2a4951c2b1008bf8c045b98cc97178e9d8fef
Vinicius Hespanha Souza <mail@vhespanha.dev>
2026-08-02 13:03:57 -0300

    remove `jsr:` import from tests
    Signed-off-by: Vinicius Hespanha Souza <mail@vhespanha.dev>

--- tests/errors.test.jsx
+++ tests/errors.test.jsx
@@ -1,4 +1,4 @@
-import { assertEquals, assertRejects } from "jsr:@std/assert";
+import { assertEquals, assertRejects } from "@std/assert";
 
 Deno.test("Skip invalid attributes", async () => {
   const props = {
